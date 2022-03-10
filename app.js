12175dbd0f1d5e9d42baa2ede5de8cff871d6589
ma7moudk3 <mkk28112000@gmail.com>
2022-03-10 15:25:01 +0200

    Static Files in Public

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended : false}));
 
 app.use('/admin',adminRoute);
 app.use(shopRoute);
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use((req,res,next) => {
 res.status(404).sendFile(path.join(__dirname,"views","404.html"))
