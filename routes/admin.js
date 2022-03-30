d931a1d8ae0258c720b6eaeea421d68f9916e347
ma7moudk3 <mkk28112000@gmail.com>
2022-03-30 16:11:50 +0300

    handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -1,31 +1,23 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
-const rootDir = require('../utils/path');
 
 const products = [];
 
-router.get("/add-product",(req,res,next)=>{
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
+});
 
- //   res.send('<form method="POST" action="/admin/product"> <input type="text" name="title"/> </form>');
-
- //res.sendFile(path.join(rootDir,"views","add-product.html"));
- res.render('../views/add-product');
-
-}); 
-
-router.post("/add-product",(req,res,next)=>{
-products.push({'title':req.body.title})
-res.redirect('/');
-}); 
-
-
-
-// app.use((req,res,next)=>{
-//     console.log('MiddleWare');
-//     next(); // Allow to move to second middlleware
-// }); //midlleware
-
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
 
 exports.routes = router;
 exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,18 +1,22 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
-const rootDir = require('../utils/path');
 
-const adminData = require("./admin");
-
-router.get("/",(req,res,next)=>{
-    console.log(adminData.products);
-   // res.send("<h1>Hi From express<h1/>");
-  // res.sendFile(path.join(rootDir,'views','shop.html')); // dirname global var that's hold obolute path on OS
+router.get('/', (req, res, next) => {
   const products = adminData.products;
-
-  res.render('../views/shop_bug', {prods: products, docTitle: 'Shop'});
-
-}); 
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
 
 module.exports = router;
